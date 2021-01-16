0d0941ad002f00242dbffad0e38104024d62fe46
ConnorMul <mulhollandconnor@icloud.com>
2021-01-16 15:12:11 -0500

    commit tree 79a02cfce836e19bba8c07c967e8d9171bdc18f2

--- src/components/PokemonCollection.js
+++ src/components/PokemonCollection.js
@@ -2,10 +2,24 @@ import React from "react";
 import PokemonCard from "./PokemonCard";
 import { Card } from "semantic-ui-react";
 
-function PokemonCollection() {
+function PokemonCollection({ pokemon, sprite, onPokeClick }) {
+  
+  const mappedPokemon = pokemon.map((poke) => {
+    return (
+    <PokemonCard 
+      key={poke.id}
+      name={poke.name}
+      hp={poke.hp}
+      sprites={poke.sprites}
+      sprite={sprite}
+      onPokeClick={onPokeClick}
+    />)
+  })
+  
   return (
     <Card.Group itemsPerRow={6}>
       <h1>Hello From Pokemon Collection</h1>
+      {mappedPokemon}
     </Card.Group>
   );
 }
--- src/components/PokemonPage.js
+++ src/components/PokemonPage.js
@@ -1,19 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PokemonCollection from "./PokemonCollection";
 import PokemonForm from "./PokemonForm";
 import Search from "./Search";
 import { Container } from "semantic-ui-react";
 
 function PokemonPage() {
+  const [pokemon, setPokemon] = useState([])
+  const [sprite, setSprite] = useState("front")
+  const [search, setSearch] = useState("")
+
+  useEffect(() => {
+    fetch("http://localhost:3001/pokemon")
+    .then(resp => resp.json())
+    .then(pokemonObjs => setPokemon(pokemonObjs))
+  }, [])
+
+  const searchedPokemon = pokemon.filter((poke) =>
+     poke.name.includes(search)
+  )
+
+  function handleAddPokemon(newPokemon) {
+    setPokemon([...pokemon, newPokemon])
+  }
+
   return (
     <Container>
       <h1>Pokemon Searcher</h1>
       <br />
-      <PokemonForm />
+      <PokemonForm 
+        onFormSubmit={handleAddPokemon}
+      />
       <br />
-      <Search />
+      <Search 
+        search={search}
+        setSearch={setSearch}
+      />
       <br />
-      <PokemonCollection />
+      <PokemonCollection 
+        pokemon={searchedPokemon}
+        sprite={sprite}
+        onPokeClick={setSprite}
+      />
     </Container>
   );
 }
